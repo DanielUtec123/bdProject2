b42c5471641b6a1629b04fd619835b9e43745e8f
FJ98 <felix.solano@utec.edu.pe>
2019-11-03 17:15:39 -0500

    tfidf

--- web/text_query.py
+++ web/text_query.py
@@ -1,114 +1,116 @@
 import nltk
-import time
 from nltk.corpus import stopwords
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+import math
 import json
-from math import log, sqrt
-# default dict se usa para evitar crear una nueva entrada vacia de diccionario cada vez que haya una nueva palabra
-from collections import defaultdict
-
-#funcion para contar el numero de doc y el numero de terminos
-
-terms = set()
-tf = {}
-query_and_terms = set()
-doc_freq = {} # para cada termino, en cuantos documentos aparece
+import numpy as np
 
 
-N = 20 # cantidad de docs
-M = 22 #cantidad de terminos
-
-def search_doc(id):
-    return id
-
-def do_query(query):
-    query_terms_vector = stem_and_tokenize(query)
-    file = open("test.json", "r")
-
-    for i in range(N):
-        j = file.readline()
-        d = json.loads(j)
-        s = stem_and_tokenize(d['text'])
-        j = file.readline()
-        j = file.readline()
-        query_and_terms = query_terms_vector.intersection(terms)
-
-
-
-def compute_terms():
-    file = open("test.json", "r")
-
-    for i in range(N):
-        j = file.readline()
-        doc = json.loads(j)
-        tf[doc['id']] = {}
-        s = stem_and_tokenize(doc['text'])
-        for i in s:
-            terms.add(i)
-        j = file.readline()
-        j = file.readline()
-    file.close()
-
-
-def term_frequency(term):
-    file = open("test.json", "r")
-
-    for i in range(N):
-        j = file.readline()
-        doc = json.loads(j)
-        s = stem_and_tokenize(doc['text'])
-        term_freq(term, s)
-        j = file.readline()
-        j = file.readline()
-    file.close()
-
-
-def stem_and_tokenize(text):
+# ---------------------------------------------BEGIN-OF-FUNCTIONS-------------------------------------------------------
+def list_stem_and_tokenize(text):
     tkn_list = nltk.word_tokenize(text)
-    ps = nltk.stem.snowball.SnowballStemmer("spanish")
-    stop_words = set(stopwords.words('spanish'))
+    ps = nltk.stem.snowball.SnowballStemmer('spanish')
+    stop_words = stopwords.words('spanish')
     filtered = [w for w in tkn_list if w not in stop_words]
 
-    my_result = set()
+    my_result = []
     for word in filtered:
-        my_result.add(ps.stem(word))
+        ls = ps.stem(word)
+        myStr = ''.join(ls)
+        my_result.append(myStr)
     return my_result
 
 
-def term_freq(term, doc):
-    count  = 0
-    for word in doc:
-        if word == term:
-            count = count + 1
-    return count
+def idf(index, term):
+    _, n_docs = index.shape
+    df = np.count_nonzero(index.loc[term])
+    return math.log(float(n_docs) / df)
 
 
-def doc_freq(doc):
-    query_terms_vector = stem_and_tokenize(doc)
-    file = open("test.json", "r")
-    length = 0
-    for i in range(N):
-        j = file.readline()
-        d = json.loads(j)
-        s = stem_and_tokenize(d['text'])
-        file.readline()
-        file.readline()
-        query_and_terms = query_terms_vector.intersection(terms)
-       ''' for word in doc_vector:
-            frequency = doc_vector[word]
-            score = tf_idf_score(word, frequency)
-            doc_vector[word] = score
-            length += score ** 2
-        length = sqrt(length)
-        for word in doc_vector:
-            doc_vector[word] /= length'''
-    file.close()
-
-def tf_idf_score(term, doc):
-    return log(1 + term_freq(term, doc)) * log(N / doc_freq[term])
+def simple_dot_product(a, b):
+    dsum = 0.
+    for ((idx,), val) in np.ndenumerate(a):
+        dsum += float(val) * float(b[idx])
+    return dsum
 
 
-compute_terms()
-print(terms)
-n  = str(input())
-print(stem_and_tokenize(n))
+def l2_norm(a): return math.sqrt(np.dot(a, a))
+
+
+def cosine_similarity(a, b): return np.dot(a, b) / (l2_norm(a) * l2_norm(b))
+
+
+def tfidf(tf, IDF): return tf * IDF
+
+
+def query_vector(theIndex, query_terms):
+    query_terms = set(query_terms)
+    n_terms, _ = theIndex.shape
+    query_vec = np.zeros(n_terms)
+
+    for idx, term in enumerate(theIndex.index):
+        if term in query_terms:
+            query_vec[idx] = idf(theIndex, term)
+
+    return query_vec
+
+
+def query(index, query_terms):
+    q = query_vector(index, query_terms)
+    n_terms, _ = index.shape
+
+    results = []
+    for idoc in index:
+        doc_vec = np.zeros(n_terms)
+
+        for (idx, (term, tf)) in enumerate(index[idoc].iteritems()):
+            doc_vec[idx] = tf * idf(index, term)
+
+        results.append((idoc, cosine_similarity(q, doc_vec)))
+
+    return sorted(results, key=lambda t: t[1], reverse=True)
+
+# -----------------------------------------------END-OF-FUNCTIONS-------------------------------------------------------
+
+
+# ----------------------------BEGIN-OF--PREPROCESSING-AND-SAVING-IN-VARIABLE-MYLIST-------------------------------------
+file = open("test.json", "r")
+
+myList = list()
+for x in range(2):
+    j = file.readline()
+    doc = json.loads(j)
+    s = list_stem_and_tokenize(doc['text'])
+    list_stem_and_tokenized_words_in_the_documents = ' '.join(s)
+    myList.append(list_stem_and_tokenized_words_in_the_documents)
+    file.readline()
+    file.readline()
+file.close()
+# ----------------------------END-OF--PREPROCESSING-AND-SAVING-IN-VARIABLE-MYLIST-------------------------------------
+
+# -------------------------------------BEGIN-OF-BUILDING-INDEX-TABLE--------------------------------------------------
+# uso la libreria sklearn
+tfidf = TfidfVectorizer()
+
+vectorsTfIdf = tfidf.fit_transform(myList)
+feature_names = tfidf.get_feature_names()
+feature_names_list = list(feature_names)
+dense = vectorsTfIdf.todense()
+denselist = dense.tolist()
+
+# uso la libreria pandas
+x = np.array(dense)
+indexTable = pd.DataFrame(x, columns=feature_names)
+transIndTable = indexTable.transpose()  # esta es la tabla de indices que siempre uso
+# -------------------------------------END-OF-BUILDING-INDEX-TABLE--------------------------------------------------
+
+# ---------------------------------------BEGIN-OF-PROGRAM-----------------------------------------------------
+myQuery = str(input("Insert Query: "))
+queryTerms = list_stem_and_tokenize(myQuery)
+
+result = query(transIndTable, queryTerms)
+
+for z in result:
+    print(z)
+# ---------------------------------------END-OF-PROGRAM-----------------------------------------------------
